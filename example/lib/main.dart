a4c3f7c390a8ce6c8b0080e574771ab8ef180b2b
Marcin Dłubakowski <marcin.dlubakowski@htdevelopers.com>
2021-04-14 22:06:08 +0200

    Add subscriber name badge to subscriber view

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:vonage_video/vonage.dart';
 import 'package:vonage_video/vonage_video.dart';
 import 'package:vonage_video_example/tokens.dart';
 
@@ -36,57 +37,78 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: AnimatedBuilder(
-          animation: vonage,
-          builder: (context, snapshot) {
-            return Stack(
-              children: [
-                if (vonage.subscribers.isNotEmpty)
-                  VonageSubscriberVideo(id: vonage.subscribers.first.id),
-                if (vonage.isPublishing)
-                  Positioned(
-                    right: 10,
-                    top: 10,
-                    child: Container(
-                      width: 120,
-                      height: 200,
-                      child: VonagePublisherVideo(),
+        body: SafeArea(
+          child: AnimatedBuilder(
+            animation: vonage,
+            builder: (context, snapshot) {
+              return Stack(
+                children: [
+                  if (vonage.subscribers.isNotEmpty)
+                    _subscriberView(vonage.subscribers.first),
+                  if (vonage.isPublishing)
+                    Positioned(
+                      right: 10,
+                      top: 10,
+                      child: Container(
+                        width: 120,
+                        height: 200,
+                        child: VonagePublisherVideo(),
+                      ),
+                    ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FloatingActionButton(
+                            onPressed: () => vonage.toggleVideoPublishing(),
+                            child: Icon(
+                              vonage.videoEnabled
+                                  ? Icons.videocam
+                                  : Icons.videocam_off,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FloatingActionButton(
+                            onPressed: () => vonage.toggleAudioPublishing(),
+                            child: Icon(
+                              vonage.audioEnabled
+                                  ? Icons.volume_up
+                                  : Icons.volume_off,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: FloatingActionButton(
-                          onPressed: () => vonage.toggleVideoPublishing(),
-                          child: Icon(
-                            vonage.videoEnabled
-                                ? Icons.videocam
-                                : Icons.videocam_off,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: FloatingActionButton(
-                          onPressed: () => vonage.toggleAudioPublishing(),
-                          child: Icon(
-                            vonage.audioEnabled
-                                ? Icons.volume_up
-                                : Icons.volume_off,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            );
-          },
+                ],
+              );
+            },
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _subscriberView(VonageSubscriber subscriber) {
+    return Stack(
+      children: [
+        VonageSubscriberVideo(id: subscriber.id),
+        Container(
+          color: Color.fromRGBO(0, 0, 0, 0.7),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              subscriber.name,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
